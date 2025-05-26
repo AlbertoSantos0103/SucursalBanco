86866edf9857d84a0bbaf6539f0b882d97fde9b9
AlbertoSantos0903 <albertosanpor@gmail.com>
2025-05-26 09:00:43 +0200

    proyecto

---
+++ sucursalbanco/src/main/java/com/sucursalbanco/Cuenta.java
@@ -0,0 +1,10 @@
+package com.sucursalbanco;
+
+import java.util.List;
+
+public class Cuenta {
+    private String numero;
+    private float saldo;
+    private String tipo;
+    private List <Movimiento> mvtos;
+}
---
+++ sucursalbanco/src/main/java/com/sucursalbanco/Movimiento.java
@@ -0,0 +1,9 @@
+package com.sucursalbanco;
+
+import java.util.Date;
+
+public class Movimiento {
+    private int id;
+    private Date fecha;
+    private float monto;
+}
